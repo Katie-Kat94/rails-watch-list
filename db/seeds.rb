89927871d5af9b0f201793f2d280a712e1c7b566
Katie Hulme <katiehulme26@hotmail.com>
2022-08-26 16:52:14 +0100

    almost done, just nedd style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# require 'open_uri'
-# require 'json'
-
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 
 movie_serialized = URI.open(url).read
